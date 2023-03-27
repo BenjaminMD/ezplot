a28b5e47b385577ffa25a415821cbe9ed7dc2570
Benjamin Fahl <Benjamin.Fahl@rwth-aachen.de>
2023-03-27 15:47:51 +0200

    added basic function and removed pdf specific functions

--- ezplot.py
+++ ezplot.py
@@ -1,137 +1,83 @@
-from matplotlib.gridspec import GridSpec
-import matplotlib.pyplot as plt
-import numpy as np
-from typing import Tuple, Union, List
-from nptyping import NDArray, Shape, Float, String
+from typing import Tuple  # type: ignore
+
+from matplotlib.gridspec import GridSpec  # type: ignore
+import matplotlib.pyplot as plt  # type: ignore
+import numpy as np  # type: ignore
 
 
-def plotdefaults(COLS, ROWS, size=15) -> Tuple[plt.Figure, GridSpec]:
+def plot_defaults(cols, rows, width_in_cm=15) -> Tuple[plt.Figure, GridSpec]:
+    """
+    Sets up plot defaults for a grid of subplots.
+    Args:
+        cols (int): Number of columns in the grid.
+        rows (int): Number of rows in the grid.
+        width (int, optional): Width of the plot in centimeters. Default is 15.
+    Returns:
+        Tuple[plt.Figure, GridSpec]: A tuple containing figure and grid spec.
+    """
     φ: float = (1 + 5**0.5)/2
-    cm: float = 2.54
+    cm_to_in: float = 2.54
 
-    WIDTH: float = size / cm
-    HEIGHT: float = WIDTH / φ
-
-    GitHub: str = 'https://raw.githubusercontent.com/BenjaminMD'
-    path: str = f'{GitHub}/MplStyle/main/style.rc'
+    width: float = width_in_cm / cm_to_in
+    height: float = width / φ
+    github: str = 'https://raw.githubusercontent.com/benjaminmd'
+    path: str = f'{github}/mplstyle/main/style.rc'
     plt.style.use(path)
 
-    fig: plt.figure = plt.figure(figsize=(WIDTH, HEIGHT))
-    gs: GridSpec = GridSpec(
-        COLS, ROWS, figure=fig,
+    fig: plt.figure = plt.figure(figsize=(width, height))
+    grid_spec: GridSpec = GridSpec(
+        cols, rows, figure=fig,
         left=0.15, right=0.85,
         bottom=0.15,  top=0.85,
         wspace=0.05 / φ, hspace=0.05
     )
 
-    return fig, gs
+    return fig, grid_spec
 
 
-def single_figure(xlabel, ylabel) -> Tuple[plt.Figure, plt.Axes]:
-    fig: plt.Figure
-    gs: GridSpec
-    ax: plt.Axes
-    fig, gs = _plotdefaults(1, 1)
+def create_basic_plot(xlabel: str, ylabel: str, title=None):
+    fig, gs = plot_defaults(1, 1)
     ax = fig.add_subplot(gs[0, 0])
-    ax.grid(True)
+
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
-    return fig, ax
-
-
-def dual_figure() -> Tuple[plt.Figure, plt.Axes]:
-    fig, gs = _plotdefaults(2, 1)
-    ax_left = fig.add_subplot(gs[0, 0])
-    ax_right = fig.add_subplot(gs[1, 0])
-    ax_left.grid(True, zorder=-10.0)
-    axs = (ax_left, ax_right)
-    return fig, axs
-
-
-def single_dual_axis_figure() -> Tuple[plt.Figure, plt.Axes]:
-    fig, gs = _plotdefaults(1, 1)
-    ax_left = fig.add_subplot(gs[0, 0])
-    ax_left.grid(True, zorder=-10.0)
-    ax_right = ax_left.twinx()
-    axs = (ax_left, ax_right)
-    return fig, axs
-
-
-def single_plot(
-        xs: NDArray[Shape["1, Dim"], Float],
-        ys: NDArray[Shape["1, Dim"], Float],
-        labels: NDArray[Shape["Dim"], String],
-        x_label: str,
-        y_label: str,
-) -> Tuple[plt.Figure, plt.Axes]:
-    fig: plt.Figure
-    ax: plt.Axes
-    fig, ax = _single_figure(x_label, y_label)
-    for x, y, label in zip(xs, ys, labels):
-        ax.plot(x, y, label=label)
-    return fig, ax
-
-
-def single_plot_dual_axis():
-    fig, ax = _single_figure(r'$r$ [Å]', r'G($r$) [-]')
-
-
-def single_pdf(
-        r: NDArray[Shape["1"], Float],
-        gcalc: NDArray[Shape["1"], Float],
-        gobs: NDArray[Shape["1"], Float],
-        filepath: str
-) -> Tuple[plt.Figure, plt.Axes]:
-    fig: plt.Figure
-    ax: plt.Axes
-    fig, ax = _single_figure(r'$r$ [Å]', r'G($r$) [-]')
-
-    gdiff: NDArray[Shape["1"], Float] = gobs - gcalc
-    span: float = max(gobs.max() - gobs.min(), gcalc.max() - gcalc.min()).max()
-    baseline: float = min(gobs.min(), gcalc.min()) - span/10
-
-    ax.scatter(r, gobs, 11, "0.0", lw=1.5)
-    ax.scatter(r, gobs, 11, "1.0", lw=0)
-    ax.scatter(r, gobs, 10, "C4", lw=0, label='obs', alpha=0.1)
-    ax.plot(r, gcalc, '-', label='calc')
-    ax.plot(r, gdiff + baseline, '-', label='diff', color='green')
-    ax.set_xlim(r.min(), r.max())
-    ax.legend()
+    if title:
+        ax.set_title(title)
 
     return fig, ax
 
 
-def stacked_single_pdf(
-        rs: List[List[Float]],
-        gcalcs: List[List[Float]],
-        gobss: List[List[Float]],
-        names: List[str],
-        filepath: str
-) -> Tuple[plt.Figure, plt.Axes]:
-    labels = {'calc': names, 'obs': 'obs', 'diff': 'diff'}
+def create_dual_plot(xlabel, y1label, y2label, y2color='red'):
+    fig, ax_main = create_basic_plot(xlabel, y1label)
+    ax_right = ctwinx(ax_main, y2color, y2label)
+    return fig, ax_main, ax_right
 
-    fig: plt.Figure
-    ax: plt.Axes
-    fig, ax = _single_figure(r'$r$ [Å]', r'G($r$) [-]')
-    global_max = np.max([np.max(gobss), np.max(gcalcs)])
-    global_min = np.min([np.min(gobss), np.min(gcalcs)])
-    for i, (r, gcalc, gobs) in enumerate(zip(rs, gcalcs, gobss)):
-        gdiff: NDArray[Shape["1"], Float] = gobs - gcalc
-        span: float = max(gobs.max() - gobs.min(),
-                          gcalc.max() - gcalc.min()).max()
-        baseline: float = min(gobs.min(), gcalc.min()) - span/10
-        shift = (global_max - global_min - baseline) * i
-        ax.scatter(r, gobs + shift, 11, "0.0", lw=1.5)
-        ax.scatter(r, gobs + shift, 11, "1.0", lw=0)
-        ax.scatter(r, gobs + shift, 10, "C4", lw=0,
-                   label=labels['obs'], alpha=0.1)
-        ax.plot(r, gdiff + baseline + shift, '-',
-                label=labels['diff'], color='green')
-        ax.plot(r, gcalc + shift, '-', label=labels['calc'][i])
-        labels = {k: None if type(v) != list else v for k, v in labels.items()}
-    ax.set_xlim(r.min(), r.max())
 
-    handles, labels = plt.gca().get_legend_handles_labels()
-    ax.legend(handles[::-1], labels[::-1], loc='upper right')
+def gather_legend(axs):
+    handles, labels = zip(*[ax.get_legend_handles_labels() for ax in axs])
+    handles = np.concatenate(handles)
+    labels = np.concatenate(labels)
+    return handles, labels
 
-    return fig, ax
+
+def ctwinx(ax_main, color, ylabel):
+    """Create a twin axis with a colored label.
+    Parameters:
+    -----------
+    ax_main : matplotlib axis object
+        The main axis to which the twin axis will be twinned.
+    color : str
+        The color of the label and tick marks.
+    label : str
+        The label to be displayed on the twin axis.
+    Returns:
+    --------
+    ax_r : matplotlib axis object
+        The twin axis with the specified label and color.
+    """
+    ax_r = ax_main.twinx()
+    ax_r.set_ylabel(ylabel, color=color)
+    ax_r.tick_params(axis='y', colors=color)
+    return ax_r
+
+
